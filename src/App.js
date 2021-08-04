d46245f58f31a6ce530fa5a272d9d448c698f4f5
Ann Vasilyeva <skazkadreamanna@gmail.com>
2021-08-04 20:15:00 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    
+        // eslint-disable-next-line jsx-a11y/anchor-has-content
+        <a
+          className="neon-button"
+          href="#https://codepen.io/kevinpowell/pen/QWdRzON"
+        >Neon</a>
+         
+  );
+}
+
+export default App;
